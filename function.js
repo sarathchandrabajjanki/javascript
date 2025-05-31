cea6e727a2ee76876b09bea41bb7e6cfe13b3859
sarathchandrabajjanki <sarathchandrabajjanki@gmail.com>
2025-05-31 10:39:30 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,80 @@
+// // // function add(a,b){
+// // //     console.log(a+b);
+// // //     console.log(a*b);
+// // //     console.log(a%b);
+// // //     console.log(a-b);
+// // //     console.log(a/b);
+// // //     console.log(a,b);
+
+    
+    
+    
+// // // }
+
+ 
+// // //  add(10,20)
+// // ////types of function
+// // 1.length        11.split
+// // 2.charAt        12.
+// // 3.indexOf
+// // 4.includes
+// // 5.startswith
+// // 6.endswith
+// // 7.replace
+// // 8.touppercase
+// // 9.tolowercase
+// // 10.trim
+// var s="sarath"
+// var S="SARATH"
+// var a="      king         "
+// console.log(s.length);
+// console.log(s.charAt(1));
+// console.log(s.indexOf("a"));
+// console.log(s.includes("sa"));
+// console.log(s.includes("as"));
+// console.log(s.startsWith("sa"));
+// console.log(s.startsWith("th"));
+// console.log(s.endsWith("th"));
+// console.log(s.endsWith("sa"));
+// console.log(s.replace("sa","ki"));
+// console.log(s.toUpperCase());
+// console.log(S.toLowerCase());
+// console.log(a.trim());
+// console.log(a);
+
+
+
+
+
+
+function sarath(a,b){
+    console.log(a/b);
+    console.log(a%b);
+    
+    
+}
+sarath(2,10)
+var a="sarathchandra"
+console.log(a.toUpperCase());
+var b="KING"
+console.log(b.toLowerCase());
+var k=[1,2,3,4,5,"javascript"]
+console.log(k[5].toUpperCase());
+var s={key:"sarath",
+    sam:"king"
+}
+console.log(s.sam[1].toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
